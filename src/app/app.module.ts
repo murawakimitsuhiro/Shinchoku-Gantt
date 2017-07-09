eb6f0183c8929f91f3b8d8542a5c3966080dbd69
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2017-07-10 02:45:57 +0900

    Add ScheduleUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import * as jQuery from "jquery"
+
 import { AppComponent } from './app.component';
+
+import {AccordionModule} from 'primeng/primeng';     //accordion and accordion tab
+import {  ButtonModule,
+          CheckboxModule,
+          ConfirmDialogModule, 
+          DialogModule,
+          FileUploadModule,
+          InputTextModule, 
+          InputMaskModule,          
+          OverlayPanelModule,
+          ScheduleModule } from 'primeng/primeng';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    CheckboxModule,
+    ConfirmDialogModule, 
+    DialogModule,
+    FileUploadModule,
+    InputTextModule, 
+    InputMaskModule,
+    OverlayPanelModule,          
+    ScheduleModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
