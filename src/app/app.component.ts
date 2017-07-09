7b7a6afd49254e05e6a0bc0ab347ac18492d62d8
murawaki-村脇 <mitsuhiromurawaki@gmail.com>
2017-07-10 03:46:39 +0900

    Add edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,41 +7,27 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'app';
+  title = 'タスク管理';
 
   events: any[];
-
   header: any;
-
   event: MyEvent;
-
   dialogVisible: boolean = false;
-
   idGen: number = 100;
 
   ngOnInit() {
     this.events = [
       {
-        "title": "All Day Event",
-        "start": "2016-01-01"
+        "id": 0,
+        "title": "iOS PDFライブラリ",
+        "start": "2017-07-01",
+        "end": "2017-07-02"
       },
       {
-        "title": "Long Event",
-        "start": "2016-01-07",
-        "end": "2016-01-10"
-      },
-      {
-        "title": "Repeating Event",
-        "start": "2016-01-09T16:00:00"
-      },
-      {
-        "title": "Repeating Event",
-        "start": "2016-01-16T16:00:00"
-      },
-      {
-        "title": "Conference",
-        "start": "2016-01-11",
-        "end": "2016-01-13"
+        "id": 1,
+        "title": "情報基礎",
+        "start": "2017-07-07",
+        "end": "2017-07-12"
       }
     ];
 
@@ -61,17 +47,28 @@ export class AppComponent {
     //this.cd.detectChanges();
   }
 
-  handleEventClick(event) {
+  handleEventClick(e) {
     this.event = new MyEvent();
-    this.event.start = event.date.format();
-    this.dialogVisible = true;
+    this.event.title = e.calEvent.title;
 
-    //trigger detection manually as somehow only moving the mouse quickly after click triggers the automatic detection
-    //this.cd.detectChanges();
+    let start = e.calEvent.start;
+    let end = e.calEvent.end;
+    if(e.view.name === 'month') {
+      start.stripTime();
+    }
+
+    if(end) {
+      end.stripTime();
+      this.event.end = end.format();
+    }
+
+    this.event.id = e.calEvent.id;
+    this.event.start = start.format();
+    this.event.allDay = e.calEvent.allDay;
+    this.dialogVisible = true;
   }
 
   saveEvent() {
-    /*
     //update
     if(this.event.id) {
       let index: number = this.findEventIndexById(this.event.id);
@@ -87,18 +84,15 @@ export class AppComponent {
     }
 
     this.dialogVisible = false;
-    */
   }
 
 
   deleteEvent() {
-    /*
     let index: number = this.findEventIndexById(this.event.id);
     if(index >= 0) {
       this.events.splice(index, 1);
     }
     this.dialogVisible = false;
-    */
   }
 
   findEventIndexById(id: number) {
@@ -115,9 +109,11 @@ export class AppComponent {
 }
 
 export class MyEvent {
+    id: number;
     title: string;
     start: string;
     end: string;
+    allDay: boolean = true;
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import * as jQuery from "jquery"
 
 import { AppComponent } from './app.component';
 
 import {AccordionModule} from 'primeng/primeng';     //accordion and accordion tab
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {  ButtonModule,
           CheckboxModule,
           ConfirmDialogModule, 
@@ -14,6 +16,7 @@ import {  ButtonModule,
           InputTextModule, 
           InputMaskModule,          
           OverlayPanelModule,
+          CalendarModule,
           ScheduleModule } from 'primeng/primeng';
 
 
@@ -23,6 +26,8 @@ import {  ButtonModule,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     ButtonModule,
     CheckboxModule,
     ConfirmDialogModule, 
@@ -30,6 +35,7 @@ import {  ButtonModule,
     FileUploadModule,
     InputTextModule, 
     InputMaskModule,
+    CalendarModule,
     OverlayPanelModule,          
     ScheduleModule  
   ],
